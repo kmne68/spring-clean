b638ff0d684453f342a93e78a49a813edcebbfe1
Keith Emery <kmne68@gmail.com>
2017-05-19 18:31:25 -0500

    Add custom email validation

---
+++ src/springclean/spring/web/validation/ValidEmailImplement.java
@@ -0,0 +1,32 @@
+package springclean.spring.web.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.apache.commons.validator.routines.EmailValidator;
+
+public class ValidEmailImplement implements ConstraintValidator<ValidEmail, String> {
+
+	private int min;
+	
+	@Override
+	public void initialize(ValidEmail constraintAnnotation) {
+		min = constraintAnnotation.min();
+		
+	}
+
+	@Override
+	public boolean isValid(String email, ConstraintValidatorContext context) {
+		
+		if(email.length() < min) {
+			return false;
+		}
+		
+		if(!EmailValidator.getInstance(false).isValid(email)) {
+			return false;
+		}
+		return true;
+				
+	}
+
+}
